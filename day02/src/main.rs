f602ca4d454ce8dde8e9c2d957c0fc723acd3ce7
Alexander Hill <CodeBreadpudding@gmail.com>
2019-12-02 01:50:25 -0500

    Realized my mistake and published the ACTUAL solution

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -2,8 +2,7 @@ use std::env;
 use std::fs::File;
 use std::io::Read;
 
-fn execute(memory: Vec<isize>) -> isize {
-	let mut mem = memory.clone();
+fn execute(mut mem: Vec<isize>) -> isize {
 	let mut ptr = 0;
 	while mem[ptr] != 99 {
 		let (a, b, c) = (mem[ptr + 1] as usize, mem[ptr + 2] as usize, mem[ptr + 3] as usize);
@@ -23,6 +22,17 @@ fn execute(memory: Vec<isize>) -> isize {
 	mem[0]
 }
 
+fn solve(memory: Vec<isize>, desired: isize) -> (isize, isize) {
+	let mut program = memory.clone();
+	program[1] = 0;
+	program[2] = 0;
+	let offset = execute(program.clone());
+	let raw = desired - offset;
+	program[1] = 1;
+	let multiplier = execute(program.clone()) - offset;
+	(raw / multiplier, raw % multiplier)
+}
+
 fn main() {
 	let args: Vec<String> = env::args().collect();
 	if args.len() == 2 {
@@ -33,7 +43,8 @@ fn main() {
 		for cell in data.split(",") {
 			memory.push(String::from(cell).trim().parse::<isize>().unwrap());
 		}
-		println!("memory[0] = {}", execute(memory));
+		let (x, y) = solve(memory, 19690720);
+		println!("Solution: ({}, {})", x, y);
 	} else {
 		println!("Usage: {} <Input File>", args[0]);
 	}
